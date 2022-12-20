117dfc187a87476b7ff7f7442ed0ebab289f5829
brenbtc <yanohb@yahoo.com>
2022-12-20 00:00:57 -0700

    Psuedo Code for all pages

--- frontend/src/Pages/movieReviews/index.js
+++ frontend/src/Pages/movieReviews/index.js
@@ -2,7 +2,18 @@ function movieReviews() {
     return(
         <div>
             {/* <Nav /> */}
-            
+            <img src="#" class="img-thumbnail" alt="" />
+            <div>
+                <section>
+                    <input>Review 1</input>
+                </section>
+                <section>
+                    <input>Review 2</input>
+                </section>
+                <section>
+                    <input>Review 3</input>
+                </section>
+            </div>
         </div>
     );
     
--- frontend/src/Pages/usersReviews/index.js
+++ frontend/src/Pages/usersReviews/index.js
@@ -1,7 +1,19 @@
 function usersReviews() {
     return(
         <div>
-            <Nav />
+            {/* <Nav /> */}
+            <img src="#" class="rounded-circle" alt="" />
+            <div>
+                <section>
+                    <input>Review 1</input>
+                </section>
+                <section>
+                    <input>Review 2</input>
+                </section>
+                <section>
+                    <input>Review 3</input>
+                </section>
+            </div>
         </div>
     );
     
