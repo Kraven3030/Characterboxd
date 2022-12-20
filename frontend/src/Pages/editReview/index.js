947c367ffbdad74a975157766ea5d08d9ffe61cf
brenbtc <yanohb@yahoo.com>
2022-12-19 23:39:49 -0700

    Edit Psuedo Code

--- frontend/src/Pages/editReview/index.js
+++ frontend/src/Pages/editReview/index.js
@@ -1,7 +1,16 @@
 function editReview() {
 return(
     <div>
-        <Nav />
+        {/* <Nav /> */}
+        <h1>Edit</h1>
+        <figure class="figure">
+            <img src="#" class="figure-img img-fluid rounded" alt='' />
+            <figcaption class="figure-caption" >
+                <input>Review Here</input>
+            </figcaption>
+        </figure>
+        <button type="submit" class="btn btn-info" >Save Changes</button>
+        <button type="submit" class="btn btn-danger" >Delete</button>
     </div>
 );
 
