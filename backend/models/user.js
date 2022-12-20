a304d8cb278f4d7ab05b646c3ec45a49bd094403
RoryV91 <vanblaricom.rory@icloud.com>
2022-12-20 09:54:26 -0800

    created schemas for user and review

---
+++ backend/models/user.js
@@ -0,0 +1,38 @@
+//==================
+//   DEPENDENCIES  
+//==================
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+//==================
+//   USER SCHEMA  
+//==================
+
+const userSchema = new Schema(
+     {
+        username: {
+            type: String,
+            unique: true,
+            required: true 
+        },
+
+        password: {
+            type: String,
+            required: true,
+        }
+    }
+);
+
+//=============================
+//   MODEL USING USER SCHEMA  
+//=============================
+
+const User = mongoose.model('User', userSchema);
+
+//===================
+//   EXPORT MODEL  
+//===================
+
+module.exports = User;
