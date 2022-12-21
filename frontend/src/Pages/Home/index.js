2d00346e6fe8e3917000eed28230ae13e46c72a2
brenbtc <yanohb@yahoo.com>
2022-12-20 23:00:13 -0700

    Home Page Structuring

--- frontend/src/Pages/Home/index.js
+++ frontend/src/Pages/Home/index.js
@@ -1,29 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './styles.css'
 
 function Home() {
+    const [media, setMedia] = useState([]);
+
+
+    
+    useEffect(() => {
+        const getMedia = async () => {
+            
+        }
+    }, []);
+
+
     return(
         <div>
             {/* <Nav /> */}
-            <h2>Track films you've watched.</h2>
-            <h2>Save those you want to see.</h2>
-            <h2>Tell your friends what's good.</h2>
-                <div id="m1">
-                    <img src="#" class="" />
-                </div>
-
-                <div id="m2">
-                <img src="#"  class="" />
-                </div>
-
-                <div id="m3">
-                <img src="#" class="" />
-                </div>
-
-                <div id="m4">
-                <img src="#" class="" />
-                </div>
-
-            <h1>Reviews</h1>
+            <section>
+                <h2>Track films you've watched.</h2>
+                <h2>Save those you want to see.</h2>
+                <h2>Tell your friends what's good.</h2>
+            </section>
+            
+                {media.map( media => {
+                    <>
+                        <img>{media.img}</img>
+                        <h2>{media.title}</h2>
+                        <h5>{media.genre}</h5>
+                    </>
+            
+                })}
 
         </div>
     );
