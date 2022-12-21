04fed84026446ce2468f9150d0de0b55db4d67c5
RoryV91 <vanblaricom.rory@icloud.com>
2022-12-21 10:00:21 -0800

    changed variable port to PORT

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT
+const PORT = process.env.PORT
 const methodOverride = require('method-override');
 
 
