d378cfaa6c9b5cca0d1083e1def22c1acfa7f208
BlakeR <blakereed3030@gmail.com>
2022-12-22 08:49:09 -0800

    nav bar code adjusted

--- frontend/src/components/Nav/nav.js
+++ frontend/src/components/Nav/nav.js
@@ -1,10 +1,14 @@
+import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
 import './nav.css'
 
 
-function Nav() {
+function Nav(props) {
+
+
+
   return (
     <div>
       <header className="nav">
