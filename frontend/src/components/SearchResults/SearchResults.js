22dc49acd1e7e4403a4d2bb651e8fec1279589bd
RoryV91 <vanblaricom.rory@icloud.com>
2022-12-22 13:53:51 -0800

    minor change to SearchResults

--- frontend/src/components/SearchResults/SearchResults.js
+++ frontend/src/components/SearchResults/SearchResults.js
@@ -3,7 +3,7 @@ import ContentResults from '../ContentResults/ContentResults';
 
 const SearchResults = ({ mediaResults }) => {
   // return early if there are no images
-  if (mediaResults.length === 0) {
+  if (mediaResults.length === 0 ) {
       return <h2>No Images Found!</h2>
   }
   return (
