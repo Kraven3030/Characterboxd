3fef9f7c0ae011e894dfd4aebe81820d07959694
brenbtc <yanohb@yahoo.com>
2023-01-02 15:15:52 -0700

    fixed api bug

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 // Axios request to create new user account
 export async function createUser(formData) {
     const { data } = await axios.post('http://localhost:9000/users/signup', formData)
@@ -12,6 +13,18 @@ export async function loginToAccount(formData) {
     return data
 }
 
+// Get user data if user is logged in
+export async function getUserData(userId) {
+    const { data } = await axios.get('http://localhost:9000/users/' + userId)
+    return data
+}
+
+// Get user personal reviews if user is logged in
+export async function userReviews(userId) {
+    const { data } = await axios.get(`http://localhost:9000/reviews/user/${userId}`)
+    return data
+}
+
 // Axios request that will allow users to create reviews once they are logged in
 export async function createReview(reviewData) {
     const config = {
@@ -19,6 +32,6 @@ export async function createReview(reviewData) {
             'Authorization': localStorage.getItem('token')
         }
     }
-    const { data } = await axios.post('http://localhost:9000/post', reviewData, config)
+    const { data } = await axios.post('http://localhost:9000/reviews/create', reviewData, config)
     return data
 }
