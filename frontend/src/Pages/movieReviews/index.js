112bd0b4e285f3d2540113dc7a0bd7d54db36cb0
BlakeR <blakereed3030@gmail.com>
2023-01-03 20:29:44 -0800

    updates

--- frontend/src/Pages/movieReviews/index.js
+++ frontend/src/Pages/movieReviews/index.js
@@ -12,38 +12,38 @@ function MovieReviews() {
     const baseUrl = "https://image.tmdb.org/t/p/original"
 
 
-        const [mediaReviews, setMediaReviews] = useState([])
-        const [mediaId, setMediaId] = useState(state.mediaId);
-    
-    
-    
-        useEffect(() => {
-             const fetchReviews = async () => {
-                 await allReviews(state.mediaId).then((res) => {
-                    setMediaReviews(res)
-                })
-    
-            }
-            
-            fetchReviews(state.mediaId)
-        }, [state.mediaId])
-        
-    
+    const [mediaReviews, setMediaReviews] = useState([])
+    const [mediaId, setMediaId] = useState(state.mediaId);
+
+
+
+    useEffect(() => {
+        const fetchReviews = async () => {
+            await allReviews(state.mediaId).then((res) => {
+                setMediaReviews(res)
+            })
+
+        }
+
+        fetchReviews(state.mediaId)
+    }, [state.mediaId])
+
+
 
     return (
         <div className="card">
-        {mediaReviews.map(review => (
-            <div className="card-body" key={review._id}>
-                <h1 className="card-title">{review.title}</h1>
-                <h2 className="card-subtitle mb-2 text-muted"> {review.body}</h2>
-                <h3 className="card-text">{review.reviewer.username}</h3>
-            </div>
-                ) )
-             
-                
-                    }
-           
-        </div>
+            {mediaReviews.map(review => (
+                <div className="card-body" key={review._id}>
+                    <h1 className="card-title">{review.title}</h1>
+                    <h2 className="card-subtitle mb-2 text-muted"> {review.body}</h2>
+                    <h3 className="card-text">{review.reviewer.username}</h3>
+                </div>
+            ))
+
+
+            }
+
+        </div >
     );
 
 }
